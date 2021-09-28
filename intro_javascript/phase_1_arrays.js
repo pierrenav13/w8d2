6274774b269f488ffdefb37c016394b8f7ee0202
Mansoo Kim <mansookim09@gmail.com>
2021-09-28 13:48:01 -0400

    Finish Array#twoSum

--- intro_javascript/phase_1_arrays.js
+++ intro_javascript/phase_1_arrays.js
@@ -7,3 +7,16 @@ Array.prototype.uniq = function () {
   });
   return uniques;
 };
+
+Array.prototype.twoSum = function () {
+  let pairs = [];
+
+  for (let i=0; i < this.length; i++) {
+    for (let j=i+1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        pairs.push([i, j])
+      }
+    }
+  }
+  return pairs;
+}
