91683338673dee2a64d4f0afd6cc72efd2afc8da
Mansoo Kim <mansookim09@gmail.com>
2021-09-28 14:44:42 -0400

    Finish mergesort

--- intro_javascript/phase_4_recursion.js
+++ intro_javascript/phase_4_recursion.js
@@ -61,3 +61,27 @@ function bsearch(arr, target) {
     return -1;
   }
 }
+
+function mergesort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  let mid = Math.floor(arr.length/2);
+  let left = mergesort(arr.slice(0, mid));
+  let right = mergesort(arr.slice(mid));
+  return merge(left, right);
+}
+
+function merge(arr1, arr2) {
+  let merged = [];
+
+  while (arr1.length > 0 && arr2.length > 0) {
+    if (arr1[0] < arr2[0]) {
+      merged.push(arr1.shift());
+    } else {
+      merged.push(arr2.shift());
+    }
+  }
+
+  return merged.concat(arr1).concat(arr2);
+}
