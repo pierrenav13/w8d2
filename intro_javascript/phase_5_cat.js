ea922b652a56a89471220384b0124408abcc667f
pierre <pnavarin@gmail.com>
2021-09-28 15:19:11 -0400

    Finished Phase 5

---
+++ intro_javascript/phase_5_cat.js
@@ -0,0 +1,13 @@
+function Cat(name, owner) {
+    this.name = name
+    this.owner = owner
+
+}
+
+Cat.prototype.cuteStatement = function() {
+    return `${this.owner} loves ${this.name}`
+}
+
+Cat.prototype.meow = function() {
+    return "meow"
+}
